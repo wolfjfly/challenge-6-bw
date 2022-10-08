9c5315a8f26db68731b8ce9a9511dc95b32ccea9
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-10-08 11:02:25 -0500

    more work done on the api some done on the html

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
-// fetch('https://maps.googleapis.com/maps/api/staticmap?center')
-// .then((response) => response.json())
-// .then((data) => console.log(data));
+// Current Weather Data = api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// q: The query parameter
+// appid: The application id or key
 
 var openKey= 'e629ac90d489be3a662623628b909aaa'
 var openAPI= 'https:api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e629ac90d489be3a662623628b909aaa'
+var hist= [];
 var lat;
 var lon;
 var id;
 
 function searchApi(userCityInput) {
-    var GeocodingApi = (`https:api.openweathermap.org/data/2.5/weather?q=${userCityInput}&appid=${openKey}`);
+    var GeocodingApi = (`https:api.openweathermap.org/data/2.5/weather?q=${userCityInput}&appid=${openKey}&units=imperial`);
     fetch(GeocodingApi)
         .then((res) => res.json())
         .then((res) => {
@@ -26,7 +27,7 @@ function searchApi(userCityInput) {
                     // var uvIndex= res[0].value;
                     //     console.log(uvIndex);
                         function forcast() {
-                            var forcastUrl= 'https:api.openweathermap.org/data/2.5/forecast?id=' + id + '&appid=' + openKey;
+                            var forcastUrl= 'https:api.openweathermap.org/data/2.5/forecast?id=' + id + '&appid=' + openKey ;
                             fetch(forcastUrl)
                                 .then((res)=> res.json())
                                 .then((res)=> {
@@ -39,33 +40,48 @@ function searchApi(userCityInput) {
             bwApi();
         });
     }
-    document.getElementById("searchBtn").addEventListener("click", function(){
-        var userCityInput = document.getElementById("UserInput").value
-        var location = searchApi(userCityInput)
-        console.log(location)
-        })
 
-    // fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shakopee&limit=5&appid==e629ac90d489be3a662623628b909aaa",{
-    //     method: 'GET',
-    //     credentials: 'same-origin',
-    //     redirect: 'follow',
-    //     })
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (data) {
-    //         console.log(data);
-    //     });
+    var searchHistoryList = function(userCityInput) {
+        $('.past-search:contains("' + userCityInput + '")').remove();
+        var searchHistoryEntry = $("<p>");
+        searchHistoryEntry.addClass("past-search");
+        searchHistoryEntry.text(userCityInput);
+        var searchEntryContainer = $("<div>");
+        searchEntryContainer.addClass("lastHistContainer");
+        searchEntryContainer.append(searchHistoryEntry);
+        var searchHistoryContainerEl = $("#histCon");
+        searchHistoryContainerEl.append(searchEntryContainer);
+        if (savedSearches.length > 0){
+            var previousSavedSearches = localStorage.getItem("savedSearches");
+            savedSearches = JSON.parse(previousSavedSearches);
+        }
+        savedSearches.push(userCityInput);
+        localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+        $("#userCityInput").val("");
+    };
+    
+    var loadSearchHistory = function() {
+        var savedSearchHistory = localStorage.getItem("savedSearches");
+        if (!savedSearchHistory) {
+            return false;
+        }
+        savedSearchHistory = JSON.parse(savedSearchHistory);
+        for (var i = 0; i < savedSearchHistory.length; i++) {
+            searchHistoryList(savedSearchHistory[i]);
+        }
+    };
 
+$("#form").on("submit", function() {
+    event.preventDefault();
+    var userCityInput = $("#userCityInput").val();
+    currentWeatherSection(userCityInput);
+    fiveDayForecastSection(userCityInput);
+});
 
-    // fetch("https:api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e629ac90d489be3a662623628b909aaa",{
-        //     method: 'GET',
-        //     credentials: 'same-origin',
-        //     redirect: 'follow',
-        //     })
-        //     .then(function (response) {
-        //         return response.json();
-        //     })
-        //     .then(function (data) {
-        //         console.log(data);
-        //     });
+$("#histCon").on("click", "p", function() {
+    var lastCityInput = $(this).text();
+    currentWeatherSection(lastCityInput);
+    fiveDayForecastSection(lastCityInput);
+    var lastCityInputClicked = $(this);
+    lastCityInputClicked.remove();
+});
